364f6a649c82a4d34a237c095e5950bcbdee8df1
AndCatr <123764247+AndCatr@users.noreply.github.com>
2025-04-01 23:00:19 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # The Fox Night Club Escape Room
-A web version of the tabletop escape room with 4 themed rooms.
+
+Web version powered by React + Vite.
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Room1 from './pages/Room1';
-import Room2 from './pages/Room2';
-import Room3 from './pages/Room3';
-import Room4 from './pages/Room4';
-
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/room1" element={<Room1 />} />
-      <Route path="/room2" element={<Room2 />} />
-      <Route path="/room3" element={<Room3 />} />
-      <Route path="/room4" element={<Room4 />} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+  return (
+    <div>
+      <h1>The Fox Night Club - Coming Soon</h1>
+    </div>
+  );
+};
 
 export default App;
