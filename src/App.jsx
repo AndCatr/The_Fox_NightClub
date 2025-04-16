ad42a5c649b2b45b298f6bef58a59b44be4a7270
AndCatr <123764247+AndCatr@users.noreply.github.com>
2025-04-16 23:07:58 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Room4 from './pages/Room4';
+import Room4Carte from './pages/Room4Carte'; // << AGGIUNTO
 
 const App = () => (
   <Router>
@@ -13,8 +14,10 @@ const App = () => (
       </nav>
       <Routes>
         <Route path="/room4" element={<Room4 />} />
+        <Route path="/room4-carte" element={<Room4Carte />} /> {/* << AGGIUNTO */}
       </Routes>
     </div>
   </Router>
 );
+
 export default App;
